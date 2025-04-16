8c812e061f5b341feb044976bd12d6f91d6d414c
Joshua Hürzeler <joshua@joshuah.ch>
2025-04-16 14:54:44 +0200

    Task 1.2 finished

--- environment.py
+++ environment.py
@@ -11,22 +11,23 @@ class Environment:
     def __init__(self, rho, ant_population):
 
         self.rho =rho
+        self.ant_population = ant_population
         
         # Initialize the environment topology
         self.problem = tsplib95.load('att48-specs/att48.tsp')
         self.graph = self.problem.get_graph()
 
         # Initialize the pheromone map in the environment
-        self.initialize_pheromone_map(ant_population)
+        self.initialize_pheromone_map()
 
         print(self.graph[1][2])
 
     # Initialize the pheromone trails in the environment
-    def initialize_pheromone_map(self, ant_population):
+    def initialize_pheromone_map(self):
         c_nn = self._calculate_nearest_neighbor_tour_length()
 
         # Initial pheromone level (τ₀) as per the Ant System algorithm
-        initial_pheromone = ant_population / c_nn
+        initial_pheromone = self.ant_population / c_nn
 
         for u, v in self.graph.edges():
             self.graph[u][v]['pheromone'] = initial_pheromone
@@ -54,8 +55,29 @@ class Environment:
         return tour_length
 
     # Update the pheromone trails in the environment
-    def update_pheromone_map(self):
-        pass
+    def update_pheromone_map(self, ant_tours):
+        # Step 1: Pheromone evaporation on all edges
+        for u, v in self.graph.edges():
+            self.graph[u][v]['pheromone'] = (1 - self.rho) * self.graph[u][v]['pheromone']
+
+        # Step 2: Add pheromone based on ant tours
+        for tour in ant_tours:
+            # Calculate total cost of the tour
+            tour_cost = 0
+            for i in range(len(tour) - 1):
+                u, v = tour[i], tour[i + 1]
+                tour_cost += self.graph[u][v]['weight']
+            # Add the return to starting point
+            tour_cost += self.graph[tour[-1]][tour[0]]['weight']
+
+            # Add pheromone to all edges in the tour
+            for i in range(len(tour) - 1):
+                u, v = tour[i], tour[i+1]
+                self.graph[u][v]['pheromone'] += 1.0 / tour_cost
+
+            # Complete the tour (return to start)
+            u, v = tour[-1], tour[0]
+            self.graph[u][v]['pheromone'] += 1.0 / tour_cost
 
     # Get the pheromone trails in the environment
     def get_pheromone_map(self):
